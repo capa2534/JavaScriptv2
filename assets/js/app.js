4d483fd9ca6328c7a14a0f82bd199ba020bbc27c
andresBolanos67 <andres.bolanos@softwareone.com>
2023-05-22 08:03:39 -0600

    all

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+alert('hi from JS');
+
+console.log(console.log('hola  mundo'));
+
+let a = 10,
+    b = 20,
+    c = 'Hola',
+    d = 'spiderman',
+    x = a + b;
+
+    const greeting = c + d;
+
+console.log('%c Variables', 'color:green; font-weight:bold');
+console.log({ a });
+console.log({ b });
+console.log({ c });
+
+c = "Hola again";
+
+console.table({a, b, c, d, x});
