8b0437f96d91f9d200f724512456cef6d0d3f820
andresBolanos67 <andres.bolanos@softwareone.com>
2023-05-22 09:05:31 -0600

    metodos viejos de window

---
+++ assets/js/alerts.js
@@ -0,0 +1,9 @@
+
+// alert('Hola world');
+
+// let name = prompt('what is your name?');
+// console.log(name);
+// console.log( '*** ' + name + ' ***' );
+
+const selection = confirm('wanna erase this?');
+console.log( selection);
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,25 @@
-alert('hi from JS');
 
-console.log(console.log('hola  mundo'));
+
+// alert('hi from JS');
+
+// console.log(console.log('hola  mundo'));
 
 let a = 10,
-    b = 20,
-    c = 'Hola',
-    d = 'spiderman',
-    x = a + b;
+b = 20,
+c = 'Hola',
+d = 'spiderman',
+x = a + b;
 
-    const greeting = c + d;
+const greeting = c + d;
 
-console.log('%c Variables', 'color:green; font-weight:bold');
-console.log({ a });
-console.log({ b });
-console.log({ c });
+// console.log('%c Variables', 'color:green; font-weight:bold');
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
 
 c = "Hola again";
 
-console.table({a, b, c, d, x});
+// console.table({a, b, c, d, x});
+
+
+let miNuevoNombre = 'Andres Bolanos ';
